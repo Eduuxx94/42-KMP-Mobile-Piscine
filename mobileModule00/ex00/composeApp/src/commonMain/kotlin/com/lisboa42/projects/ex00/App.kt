d32859a285fc89cda7436e64d975de598f4d804f
Eduardo Carneiro <eduardo.pereira.carneiro@sibs.com>
2025-10-25 16:15:06 +0100

    Added Log Object to print logs (In all modules). Removed Starting UI Components. Added ui Button and Text.

--- mobileModule00/ex00/composeApp/src/commonMain/kotlin/com/lisboa42/projects/ex00/App.kt
+++ mobileModule00/ex00/composeApp/src/commonMain/kotlin/com/lisboa42/projects/ex00/App.kt
@@ -1,48 +1,57 @@
 package com.lisboa42.projects.ex00
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.lisboa42.projects.ex00.common.Log
 import org.jetbrains.compose.ui.tooling.preview.Preview
-
-import ex00.composeapp.generated.resources.Res
-import ex00.composeapp.generated.resources.compose_multiplatform
 
 @Composable
 @Preview
 fun App() {
     MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
         Column(
             modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
+                .background(Color.White)
                 .safeContentPadding()
                 .fillMaxSize(),
             horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
         ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
+            Text("A simple Text", color = Color.White, modifier = Modifier
+                .background(color = Color(0xFF626b32), shape = RoundedCornerShape(8.dp))
+                .padding(horizontal = 8.dp, vertical = 8.dp), fontSize = 24.sp)
+            Button(
+                onClick = {
+                    Log.i("I/Kotlin", "Button pressed")
+                },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = Color(0xFF626b32),
+                ),
+                elevation = ButtonDefaults.buttonElevation(
+                    defaultElevation = 8.dp,
+                    pressedElevation = 12.dp,
+                    focusedElevation = 6.dp,
+                    hoveredElevation = 10.dp,
+                    disabledElevation = 4.dp
+                ),
+            ) {
+                Text("Click me")
             }
         }
     }
